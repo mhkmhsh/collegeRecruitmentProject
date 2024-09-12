95a3bcd042157ddde2ad79e04f9dc1af29ff264b
mhkmhsh <mmahesh12@gmail.com>
2024-09-12 14:45:19 -0400

    added more to the header and d-none on the body, have to figure out how to make it reappear

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let userInput = document.querySelector("#userInput")
+    let message = document.querySelector("#message")
+    message.innerHTML ="So " + userInput.value +", you want to go to Princeton "
+  
+  }
+  
